5bdd123bf8705fbc29fcc0a2a8f5a83bda3feafb
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-07-23 22:54:14 +0400

    slider added

---
+++ client/src/components/slider/Slider.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import required modules
+import { EffectCards, Autoplay } from "swiper/modules";
+import "./slider.scss";
+import { useState } from "react";
+
+function Slider() {
+  return (
+    <>
+      <div className="slider">
+        <div className="sliderContainer">
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards, Autoplay]}
+            className="mySwiper"
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+          >
+            <SwiperSlide></SwiperSlide>
+            <SwiperSlide></SwiperSlide>
+            <SwiperSlide></SwiperSlide>
+            <SwiperSlide></SwiperSlide>
+            <SwiperSlide> </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Slider;
