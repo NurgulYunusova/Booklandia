4ecc391b08bfb9d01d80fcbf531dafaeff8ac923
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-08-02 01:20:06 +0400

    some models, controllers, etc. added

---
+++ server/models/Book.js
@@ -0,0 +1,59 @@
+const { default: mongoose } = require("mongoose");
+
+function baseUrl(v) {
+  return "http://localhost:8080/" + v;
+}
+
+const bookSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    isbn: {
+      type: Number,
+      required: true,
+    },
+    pages: {
+      type: Number,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+        required: true,
+      },
+    ],
+    image: { type: String, get: baseUrl },
+  },
+  { timestamps: true }
+);
+
+const Book = new mongoose.model("Book", bookSchema);
+
+module.exports = {
+  Book,
+};
