05fa7699ec6f82d5dcb9647a131e00a9cf80f5a8
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-08-01 23:46:21 +0400

    added tabs infos

--- client/src/pages/profile/ProfilePage.jsx
+++ client/src/pages/profile/ProfilePage.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "./profile.scss";
 import { useState } from "react";
 import { Tab, Tabs } from "@mui/material";
+import userImage from "../../assets/images/userImage.jpg";
+import img from "../../assets/images/anna_karenina.jpg";
 
 function ProfilePage() {
   const navigate = useNavigate();
@@ -63,14 +65,115 @@ function ProfilePage() {
 
               <div className="tabContent">
                 {activeTab === 0 && (
-                  <div>
-                    <p>User's personal data goes here...</p>
+                  <div className="profile">
+                    <div className="leftSide">
+                      <div className="infos">
+                        <div className="dataNames">
+                          <p>Name</p>
+                          <p>Email</p>
+                          <p>Password</p>
+                        </div>
+
+                        <div className="datas">
+                          <p>John</p>
+                          <p>johndoe@gmail.com</p>
+                          <p>*********</p>
+                        </div>
+                      </div>
+
+                      <div className="editButton">
+                        <button>EDIT PROFILE</button>
+                      </div>
+                    </div>
+
+                    <div className="rightSide">
+                      <p>Profile image</p>
+                      <img src={userImage} alt="" />
+                      <button>UPLOAD NEW</button>
+                    </div>
                   </div>
                 )}
 
                 {activeTab === 1 && (
-                  <div>
-                    <p>User's orders go here...</p>
+                  <div className="orders">
+                    <div className="order">
+                      <div className="images">
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                      </div>
+
+                      <div className="infosAboutOrder">
+                        <div className="orderNumber">
+                          <h4>Order number</h4>
+                          <p>7681029</p>
+                        </div>
+
+                        <div className="orderDate">
+                          <h4>Order date</h4>
+                          <p>30 March 2019</p>
+                        </div>
+
+                        <div className="total">
+                          <h4>Total</h4>
+                          <p>$78.00</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="order">
+                      <div className="images">
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                      </div>
+
+                      <div className="infosAboutOrder">
+                        <div className="orderNumber">
+                          <h4>Order number</h4>
+                          <p>7681029</p>
+                        </div>
+
+                        <div className="orderDate">
+                          <h4>Order date</h4>
+                          <p>30 March 2019</p>
+                        </div>
+
+                        <div className="total">
+                          <h4>Total</h4>
+                          <p>$78.00</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="order">
+                      <div className="images">
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                        <img src={img} alt="" />
+                      </div>
+
+                      <div className="infosAboutOrder">
+                        <div className="orderNumber">
+                          <h4>Order number</h4>
+                          <p>7681029</p>
+                        </div>
+
+                        <div className="orderDate">
+                          <h4>Order date</h4>
+                          <p>30 March 2019</p>
+                        </div>
+
+                        <div className="total">
+                          <h4>Total</h4>
+                          <p>$78.00</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 )}
               </div>
