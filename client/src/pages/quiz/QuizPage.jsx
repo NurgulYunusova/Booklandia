30b1035bba3eb999617cf3ad89433093aa9a1f0e
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-08-21 00:34:27 +0400

    updated quiz page

--- client/src/pages/quiz/QuizPage.jsx
+++ client/src/pages/quiz/QuizPage.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import "./quiz.scss";
+import { UserContext } from "../../context/UserContext";
+import Loading from "../../components/loading/Loading";
 
 function QuizPage() {
+  const { isLoggedIn, user } = useContext(UserContext);
   const [quiz, setQuiz] = useState([]);
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [showResult, setShowResult] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [result, setResult] = useState({
     score: 0,
@@ -29,7 +33,7 @@ function QuizPage() {
   const totalQuestions = quiz.length > 0 ? quiz[0].totalQuestions : 0;
 
   if (questions.length === 0 || activeQuestion >= totalQuestions) {
-    return <div>Loading or No questions available</div>;
+    return <Loading />;
   }
 
   const { question, choices, correctAnswer } = questions[activeQuestion];
@@ -62,45 +66,78 @@ function QuizPage() {
     }
   };
 
+  const startQuiz = () => {
+    setActiveQuestion(0);
+    setResult({
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+    });
+    setClicked(true);
+  };
+
   const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
   return (
     <>
       <div className="quiz">
         <div className="quizContainer">
+          <div className="userGreeting">
+            {clicked ? <></> : <p>Hello, {isLoggedIn ? user.name : "Guest"}</p>}
+          </div>
           {!showResult ? (
-            <div>
-              <div>
-                <span className="activeQuestionNo">
-                  {addLeadingZero(activeQuestion + 1)}
-                </span>
-                <span className="totalQuestion">
-                  /{addLeadingZero(questions.length)}
-                </span>
-              </div>
-              <h2>{question}</h2>
-              <ul>
-                {choices.map((answer, index) => (
-                  <li
-                    onClick={() => onAnswerSelected(answer, index)}
-                    key={answer}
-                    className={
-                      selectedAnswerIndex === index ? "selectedAnswer" : null
-                    }
+            <>
+              {clicked ? (
+                <div>
+                  <div>
+                    <span className="activeQuestionNo">
+                      {addLeadingZero(activeQuestion + 1)}
+                    </span>
+                    <span className="totalQuestion">
+                      /{addLeadingZero(questions.length)}
+                    </span>
+                  </div>
+                  <h2>{question}</h2>
+                  <ul>
+                    {choices.map((answer, index) => (
+                      <li
+                        onClick={() => onAnswerSelected(answer, index)}
+                        key={answer}
+                        className={
+                          selectedAnswerIndex === index
+                            ? "selectedAnswer"
+                            : null
+                        }
+                      >
+                        {answer}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="flexRight">
+                    <button
+                      onClick={onClickNext}
+                      disabled={selectedAnswerIndex === null}
+                      className="next"
+                    >
+                      {activeQuestion === questions.length - 1
+                        ? "Finish"
+                        : "Next"}
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="buttonDiv">
+                  <button
+                    className="startButton"
+                    onClick={() => {
+                      startQuiz();
+                    }}
                   >
-                    {answer}
-                  </li>
-                ))}
-              </ul>
-              <div className="flexRight">
-                <button
-                  onClick={onClickNext}
-                  disabled={selectedAnswerIndex === null}
-                >
-                  {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
-                </button>
-              </div>
-            </div>
+                    START QUIZ
+                  </button>
+                </div>
+              )}
+            </>
           ) : (
             <div className="result">
               <h3>Result</h3>
