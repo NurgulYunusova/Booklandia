ccbcf8a325bffda164d62b45eae863be42a6f56d
Nurgul Yunusova <106387672+NurgulYunusova@users.noreply.github.com>
2023-09-10 23:29:00 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://booklandia-sepia.vercel.app/",
+    origin: "https://booklandia-sepia.vercel.app",
     credentials: true,
   })
 );
